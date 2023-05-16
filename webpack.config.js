2b270faeeeffeb9ce2f605391ccb152a55a8ae32
Momo-87 <momochristian87@gmail.com>
2023-05-16 16:02:11 +0200

    Congiguring Webpack - loading CSS and Using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: "development",
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
@@ -13,5 +16,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 }
